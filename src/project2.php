e6272e377ece3bb0398cb2ec3ac328e7e857534c
mppankov <pankov.mp@yandex.ru>
2023-10-25 16:04:33 +0300

    added a class AppleTree

--- src/project2.php
+++ src/project2.php
@@ -1,8 +1,13 @@
 <?php
 
 use Pr2\Wild\Animals\Boar;
+use Pr2\Wild\Trees\AppleTree;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $b = new Boar();
+$t = new AppleTree();
+
 echo $b->go();
+echo "/n";
+echo $t->go();
