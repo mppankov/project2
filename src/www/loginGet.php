f9f5b1433d6d91d625b06ebc7da6d3c28c28b577
mppankov <pankov.mp@yandex.ru>
2023-12-05 16:21:00 +0300

    get-post

---
+++ src/www/loginGet.php
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+<?php
+$login = $_GET['login'] ?? '';
+$password = $_GET['password'] ?? '';
+
+if ($login === 'admin' && $password === 'password') {
+    $authResult = 'Авторизация прошла успешно';
+} else if($login !== 'admin') {
+    $authResult = 'Логин неверный';
+} else {
+    $authResult = 'Пароль неверный';
+}
+?>
+<html>
+<head>
+    <title>Результат авторизации</title>
+</head>
+<body>
+<?= $authResult ?>
+</body>
+</html>
