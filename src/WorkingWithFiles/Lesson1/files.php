dbad679095bd9b6cb3b6a673a59eac98af062796
mppankov <pankov.mp@yandex.ru>
2023-12-15 14:25:30 +0300

    continue files

--- src/WorkingWithFiles/Lesson1/files.php
+++ src/WorkingWithFiles/Lesson1/files.php
@@ -1,4 +1,3 @@
 <?php
-
 $data = 'abc' . PHP_EOL . 'def' . PHP_EOL;
-file_put_contents(__DIR__ . '/file3.txt', $data);
+$data = file_put_contents(__DIR__ . '/file3.txt', $data, FILE_APPEND);
--- src/WorkingWithFiles/Lesson2/upload.php
+++ src/WorkingWithFiles/Lesson2/upload.php
@@ -1,17 +1,18 @@
 <?php
-
 if (!empty($_FILES['attachment'])) {
     $file = $_FILES['attachment'];
 
-    // собираем путь до нового файла - папка uploads в текущей директории
-    // в качестве имени оставляем исходное файла имя во время загрузки в браузере
+    
     $srcFileName = $file['name'];
-    $newFilePath = __DIR__ . 'Uploads/' . $srcFileName;
+    $newFilePath = './Uploads/' . $srcFileName;
+    //$newFilePath =  '/tmp/' . $srcFileName;
+    $error = null;
+    $result = null;
 
     if (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
         $error = 'Ошибка при загрузке файла';
     } else {
-        $result = 'http://myproject.loc/Lesson2/Uploads/' . $srcFileName;
+        $result = 'http://myproject.com/Lesson2/Uploads/' . $srcFileName;
     }
 }
 ?>
@@ -20,6 +21,12 @@ if (!empty($_FILES['attachment'])) {
     <title>Загрузка файла</title>
 </head>
 <body>
+<?php if ($error): ?>
+    <?= $error ?>
+<?php elseif ($result): ?>
+    <?= $result ?>
+<?php endif; ?>
+<br>
 <form action="/Lesson2/upload.php" method="post" enctype="multipart/form-data">
     <input type="file" name="attachment">
     <input type="submit">
